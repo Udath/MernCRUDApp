16db63d8d51cb952208e32a1b356b0a6ff1687c5
Udath Paladugu <udath.p@gmail.com>
2023-07-24 13:57:25 +0530

    Error Middleware

--- backend/middleware/errorMiddleWare.js
+++ backend/middleware/errorMiddleWare.js
@@ -1,12 +1,14 @@
-const errorHandler = (err,req,res,next)=>{
+const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
-
-    res.status(500).json({
-        message : err.message,
-        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  
+    res.status(statusCode)
+  
+    res.json({
+      message: err.message,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     })
-}
-
-module.exports = {
-    errorHandler
-}
+  }
+  
+  module.exports = {
+    errorHandler,
+  }
